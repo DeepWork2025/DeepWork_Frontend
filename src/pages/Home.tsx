f27404bca6427da0179bef1d117d46e903dbfc23
Siwi0w0 <yufei.sun64@gmail.com>
2025-03-06 10:04:45 +0000

    fix home.tsx layout issue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,23 +2,19 @@ import React, { useState } from "react";
 import Navbar from "../components/navbar/Navbar";
 import Calendar from "../components/Calendar/Calendar";
 import Timer from "../components/timer/Timer";
-import "../styles/Home.css";
 import DayCalendar from "../components/Calendar/DayCalendar";
-import Calendar from "../components/calendar/Calendar";
-import Timer from "../components/timer/Timer";
-import TimeAxis from "../components/timeAxis/TimeAxis";
 import RightSideBar from "../components/rightSideBar/RightSideBar";
 
 const Home: React.FC = () => {
   const [isSidebarExpanded, setIsSidebarExpanded] = useState(true);
 
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-screen flex flex-col overflow-hidden">
       {/* Navbar 顶部导航栏 */}
       <Navbar />
 
       {/* 主体布局区域 */}
-      <div className="flex flex-1">
+      <div className="flex flex-1 overflow-hidden">
         {/* 左侧边栏 */}
         <div
           className={`bg-gray-100 h-full transition-all duration-300 ${
@@ -38,13 +34,13 @@ const Home: React.FC = () => {
             </div>
           )}
         </div>
-        <div className="time-axis-panel">
+        <div className="flex-1 overflow-y-auto bg-white time-axis-panel">
           <DayCalendar />
 
         </div>
 
         {/* 右侧边栏 */}
-        <div className="w-[400px] bg-gray-200">
+        <div className="w-[400px] bg-gray-200 overflow-y-auto">
           <RightSideBar />
         </div>
       </div>
