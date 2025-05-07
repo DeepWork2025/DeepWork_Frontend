d80a6314bcb8a0f66aa2fdb2ff720adef3c88bb9
kiwylai <kiwylai@gmail.com>
2025-05-07 21:06:37 +0200

    create user authentication via axios

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,46 +1,45 @@
-// const API_BASE_URL = import.meta.env.VITE_API_URL;
+import axios from "axios";
 
-//  Register a new user
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+// Register a new user
 export const registerUser = async (userData: {
   username: string;
   email: string;
   password: string;
 }) => {
-  const response = await fetch(`http://127.0.0.1:8000/register/`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(userData),
-  });
-  const data = await response.json();
-  if (!response.ok) {
-    throw new Error(data.error || "Registration failed");
-  }
+  try {
+    const response = await axios.post(`${API_BASE_URL}/register/`, userData);
+    const data = response.data;
 
-  localStorage.setItem("authToken", data.token);
-  return data;
+    localStorage.setItem("authToken", data.token);
+    return data;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {  
+    const message = error.response?.data?.error || "Registration failed";
+    throw new Error(message);
+  }
 };
 
 // Login user and get token
 export const loginUser = async (username: string, password: string) => {
-  const response = await fetch(`http://localhost:8000/login/`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username, password }),
-  });
+  try {
+    const response = await axios.post(`${API_BASE_URL}/login/`, {
+      username,
+      password,
+    });
+    const data = response.data;
 
-  const data = await response.json();
-
-  // Not sure what's response.ok
-  // if (!response.ok) {
-  //   console.error("Login failed:", data);
-  //   throw new Error(data.error || "Login failed");
-  // }
-
-  localStorage.setItem("authToken", data.token);
-  return data;
+    localStorage.setItem("authToken", data.token);
+    return data;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    const message = error.response?.data?.error || "Login failed";
+    throw new Error(message);
+  }
 };
 
-//  Logout user
+// Logout user
 export const logoutUser = () => {
   localStorage.removeItem("authToken");
 };
